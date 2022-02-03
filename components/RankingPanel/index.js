509f800dd5f0c3435b940672dc7c539affb71246
Pedro Ribeiro <pedro.ribeiro@somagrupo.com.br>
2022-02-03 15:19:04 -0300

    Change the filter layout

--- components/RankingPanel/index.js
+++ components/RankingPanel/index.js
@@ -1,10 +1,24 @@
-import { Divider, Paper, TextField, Typography } from "@mui/material";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Divider,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { Box } from "@mui/system";
 import { useState } from "react";
 import CustomTable from "components/CustomTable";
 import MaskedNumberInput from "components/MaskedNumberInput";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-export default function RankingPanel({ stocks, headCells, initialOrderBy,hideYearsWithProfitFilter }) {
+export default function RankingPanel({
+  stocks,
+  headCells,
+  initialOrderBy,
+  hideYearsWithProfitFilter,
+}) {
   const [minimumMarketCap, setMinimumMarketCap] = useState("");
   const [minimumLiquidity, setMinimumLiquidity] = useState("");
   const [minimumYearsWithProfit, setMinimumYearsWithProfit] = useState("");
@@ -20,64 +34,76 @@ export default function RankingPanel({ stocks, headCells, initialOrderBy,hideYea
   };
 
   const filterByYearsWithProfit = (stock) => {
-    const minimumYearsWithProfitAsNumber = Number(minimumYearsWithProfit)
+    const minimumYearsWithProfitAsNumber = Number(minimumYearsWithProfit);
     const profitableLastYears = stock?.historicalData?.["P/L"]?.series
-      .slice(2, 2+minimumYearsWithProfitAsNumber)
+      .slice(2, 2 + minimumYearsWithProfitAsNumber)
       .filter((year) => year?.value > 0);
-    const alternativeProfitableLastYears = stock?.historicalData?.["LPA"]?.series
-      .slice(2, 2+minimumYearsWithProfitAsNumber)
+    const alternativeProfitableLastYears = stock?.historicalData?.[
+      "LPA"
+    ]?.series
+      .slice(2, 2 + minimumYearsWithProfitAsNumber)
       .filter((year) => year?.value > 0);
     return minimumYearsWithProfitAsNumber
       ? profitableLastYears?.length === minimumYearsWithProfitAsNumber ||
-      alternativeProfitableLastYears?.length === minimumYearsWithProfitAsNumber
+          alternativeProfitableLastYears?.length ===
+            minimumYearsWithProfitAsNumber
       : true;
   };
-  
+
   return (
-    <Paper sx={{ paddingTop: "24px" }}>
-      <Typography variant="h6" textAlign={"start"} ml={2} mb={2}>
-        Filtros:
-      </Typography>
-      <Box mb={4} textAlign={"start"} ml={2}>
-        <Typography>Liquidez diária mínima: (R$)</Typography>
-        <MaskedNumberInput
-          value={minimumLiquidity}
-          handleChange={(e) => setMinimumLiquidity(e.target.value)}
-          placeholder={"0"}
+    <>
+      <Accordion sx={{ marginBottom: "16px" }}>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography variant="h6">Filtros:</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Box mb={4} textAlign={"start"} ml={2}>
+            <Typography>Liquidez diária mínima: (R$)</Typography>
+            <MaskedNumberInput
+              value={minimumLiquidity}
+              handleChange={(e) => setMinimumLiquidity(e.target.value)}
+              placeholder={"0"}
+            />
+          </Box>
+          <Box mb={4} textAlign={"start"} ml={2}>
+            <Typography>Valor de mercado mínimo: (R$)</Typography>
+            <MaskedNumberInput
+              value={minimumMarketCap}
+              handleChange={(e) => setMinimumMarketCap(e.target.value)}
+              placeholder={"0"}
+            />
+          </Box>
+          {hideYearsWithProfitFilter ? (
+            <></>
+          ) : (
+            <Box mb={4} textAlign={"start"} ml={2}>
+              <Typography>Anos passados com lucro: (Anos)</Typography>
+              <TextField
+                value={minimumYearsWithProfit}
+                onChange={(e) => setMinimumYearsWithProfit(e.target.value)}
+                placeholder={"0"}
+              />
+            </Box>
+          )}
+        </AccordionDetails>
+      </Accordion>
+      <Paper sx={{ padding: "16px" }}>
+        <Typography variant="h6" textAlign={"start"} >
+          Ranking:
+        </Typography>
+        <CustomTable
+          rows={stocks
+            .filter(filterByMarketCap)
+            .filter(filterByLiquidity)
+            .filter(filterByYearsWithProfit)}
+          headCells={headCells}
+          initialOrderBy={initialOrderBy}
         />
-      </Box>
-      <Box mb={4} textAlign={"start"} ml={2}>
-        <Typography>Valor de mercado mínimo: (R$)</Typography>
-        <MaskedNumberInput
-          value={minimumMarketCap}
-          handleChange={(e) => setMinimumMarketCap(e.target.value)}
-          placeholder={"0"}
-        />
-      </Box>
-      {hideYearsWithProfitFilter ? (
-        <></>
-      ) : (
-        <Box mb={4} textAlign={"start"} ml={2}>
-          <Typography>Anos passados com lucro: (Anos)</Typography>
-          <TextField
-            value={minimumYearsWithProfit}
-            onChange={(e) => setMinimumYearsWithProfit(e.target.value)}
-            placeholder={"0"}
-          />
-        </Box>
-      )}
-      <Divider sx={{ width: "90%" }} />
-      <Typography variant="h6" textAlign={"start"} ml={2} mt={3}>
-        Ranking:
-      </Typography>
-      <CustomTable
-        rows={stocks
-          .filter(filterByMarketCap)
-          .filter(filterByLiquidity)
-          .filter(filterByYearsWithProfit)}
-        headCells={headCells}
-        initialOrderBy={initialOrderBy}
-      />
-    </Paper>
+      </Paper>
+    </>
   );
 }
