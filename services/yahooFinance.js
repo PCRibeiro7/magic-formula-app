9b3773e99e97b5f77da48544035cacdd93e8b582
Pedro Ribeiro <pedro.ribeiro@somagrupo.com.br>
2022-03-07 02:29:58 -0300

    fix: improve yahoo finance service and change api url

--- services/yahooFinance.js
+++ services/yahooFinance.js
@@ -1,12 +1,13 @@
 const yahooFinance = require("yahoo-finance");
 
-export const getHistoricalPrices = async ({ symbols, from, to }) => {
+export const getHistoricalPrices = async ({ symbols, from, to,period }) => {
   const yahooPromise = new Promise((resolve, reject) => {
     yahooFinance.historical(
       {
         symbols,
         from,
         to,
+        period
       },
       function (err, result) {
         if (err) reject(err);
