1ae2eba849d4c2754acf7d071b2292444218d711
Pedro Ribeiro <pedro.ribeiro@somagrupo.com.br>
2022-03-14 12:46:30 -0300

    fix dividends filter

--- components/FilterPanel/index.js
+++ components/FilterPanel/index.js
@@ -18,67 +18,66 @@ export const FilterPanel = function ({
   minimumYearsWithProfit,
   setMinimumYearsWithProfit,
   hideYearsWithProfitFilter,
-  showDividendFilters,
+  showDividendFilter,
   lastYears,
   setLastYears,
 }) {
-    return (
-      <Accordion sx={{ marginBottom: "16px" }}>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography variant="h6">
-            <FilterAltIcon sx={{ verticalAlign: "sub", marginRight: "8px" }} />
-            Filtros:
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
+  return (
+    <Accordion sx={{ marginBottom: "16px" }}>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        aria-controls="panel1a-content"
+        id="panel1a-header"
+      >
+        <Typography variant="h6">
+          <FilterAltIcon sx={{ verticalAlign: "sub", marginRight: "8px" }} />
+          Filtros:
+        </Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <Box mb={4} textAlign={"start"} ml={2}>
+          <Typography>Liquidez diária mínima: (R$)</Typography>
+          <MaskedNumberInput
+            value={minimumLiquidity}
+            handleChange={(e) => setMinimumLiquidity(e.target.value)}
+            placeholder={"0"}
+          />
+        </Box>
+        <Box mb={4} textAlign={"start"} ml={2}>
+          <Typography>Valor de mercado mínimo: (R$)</Typography>
+          <MaskedNumberInput
+            value={minimumMarketCap}
+            handleChange={(e) => setMinimumMarketCap(e.target.value)}
+            placeholder={"0"}
+          />
+        </Box>
+        {hideYearsWithProfitFilter ? (
+          <></>
+        ) : (
           <Box mb={4} textAlign={"start"} ml={2}>
-            <Typography>Liquidez diária mínima: (R$)</Typography>
-            <MaskedNumberInput
-              value={minimumLiquidity}
-              handleChange={(e) => setMinimumLiquidity(e.target.value)}
+            <Typography>Anos passados com lucro: (Anos)</Typography>
+            <TextField
+              value={minimumYearsWithProfit}
+              onChange={(e) => setMinimumYearsWithProfit(e.target.value)}
               placeholder={"0"}
             />
           </Box>
+        )}
+        {showDividendFilter ? (
           <Box mb={4} textAlign={"start"} ml={2}>
-            <Typography>Valor de mercado mínimo: (R$)</Typography>
-            <MaskedNumberInput
-              value={minimumMarketCap}
-              handleChange={(e) => setMinimumMarketCap(e.target.value)}
-              placeholder={"0"}
+            <Typography>
+              Número de anos passados considerados: (Anos)
+            </Typography>
+            <TextField
+              value={lastYears}
+              onChange={(e) => setLastYears(Number(e.target.value))}
+              placeholder={"3"}
             />
           </Box>
-          {hideYearsWithProfitFilter ? (
-            <></>
-          ) : (
-            <Box mb={4} textAlign={"start"} ml={2}>
-              <Typography>Anos passados com lucro: (Anos)</Typography>
-              <TextField
-                value={minimumYearsWithProfit}
-                onChange={(e) => setMinimumYearsWithProfit(e.target.value)}
-                placeholder={"0"}
-              />
-            </Box>
-          )}
-          {showDividendFilters ? (
-            <></>
-          ) : (
-            <Box mb={4} textAlign={"start"} ml={2}>
-              <Typography>
-                Número de anos passados considerados: (Anos)
-              </Typography>
-              <TextField
-                value={lastYears}
-                onChange={(e) => setLastYears(Number(e.target.value))}
-                placeholder={"3"}
-              />
-            </Box>
-          )}
-        </AccordionDetails>
-      </Accordion>
-    );
-
+        ) : (
+          <></>
+        )}
+      </AccordionDetails>
+    </Accordion>
+  );
 };
