a0d41bf243530f6c79ad434d77e325ae72a8c32b
Pedro Ribeiro <pribeiro@extensiv.com>
2023-11-02 05:41:58 -0300

    removes js  transpiled files

--- utils/supabase.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const supabase_js_1 = require("@supabase/supabase-js");
-exports.default = (0, supabase_js_1.createClient)(process.env.DATABASE_URL, process.env.SUPABASE_SERVICE_API_KEY, {
-    auth: { persistSession: false },
-    realtime: { logger: (log) => console.log(log), log_level: "debug" },
-});
