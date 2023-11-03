d95d16c0666f01f8509a2dc593bcf05e557ab6f7
Pedro Ribeiro <pribeiro@extensiv.com>
2023-11-03 00:16:30 -0300

    improve MaskedNumberInput logic

--- components/MaskedNumberInput/index.tsx
+++ components/MaskedNumberInput/index.tsx
@@ -2,42 +2,22 @@ import { TextField } from "@mui/material";
 import NumberFormat from "react-number-format";
 import React from "react";
 
-const NumberFormatCustom = React.forwardRef(function NumberFormatCustom(
-    { onChange, name, ...other }: any,
-    ref
-) {
-    return (
-        <NumberFormat
-            {...other}
-            getInputRef={ref}
-            onValueChange={(values) => {
-                onChange({
-                    target: {
-                        name: name,
-                        value: values.value,
-                    },
-                });
-            }}
-            thousandSeparator={"."}
-            decimalSeparator={","}
-            isNumericString
-            prefix="R$"
-        />
-    );
-});
-
 export default function MaskedNumberInput({
     value,
     handleChange,
     placeholder,
 }) {
     return (
-        <TextField
+        <NumberFormat
+            thousandSeparator={"."}
+            decimalSeparator={","}
+            isNumericString
+            prefix="R$"
             value={value}
-            onChange={handleChange}
-            InputProps={{
-                inputComponent: NumberFormatCustom,
-            }}
+            customInput={TextField}
+            onValueChange={({ value: v }) =>
+                handleChange({ target: { name, value: v } })
+            }
             placeholder={placeholder}
         />
     );
